7c3ec41b952f78a215243c272b8b1bff41846869
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-03-20 21:40:48 +0900

    Add testcase: example_404

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     python_requires=">=3.7",
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'pycmark>=0.9.2',
+        'pycmark>=0.9.3',
     ],
     extras_require={
         'test': [
