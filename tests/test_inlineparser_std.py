1bc22ff09c8200fb6e4c554532b946a392ab2ce1
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-03-20 22:20:56 +0900

    Add testcase: example_315

---
+++ tests/test_inlineparser_std.py
@@ -0,0 +1,15 @@
+"""
+    test_inlineparser_std
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: Copyright 2019 by Takeshi KOMIYA
+    :license: Apache License 2.0, see LICENSE for details.
+"""
+
+from docutils import nodes
+from utils import publish, assert_node
+
+
+def test_example_315():
+    result = publish("&#35; &#1234; &#992; &#0;")
+    assert_node(result, [nodes.document, nodes.paragraph, "# Ӓ Ϡ �"])
